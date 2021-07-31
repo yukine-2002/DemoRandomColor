946cc10808965ae38cc5caf88be0f08c6297279e
yukine-2002 <86049765+yukine-2002@users.noreply.github.com>
2021-07-31 23:01:44 +0700

    Update code.js

--- code.js
+++ code.js
@@ -42,7 +42,7 @@ var changeColor = (color) => {
 var setSquare = () => {
 	color = getColor(numberRam);
 	clickColors = chooseColor();
-	display.textContent = clickColors();
+	display.textContent = clickColors;
 	console.log(clickColors)
 	console.log('a' + uplevel)
 	if(numberRam === 6){
